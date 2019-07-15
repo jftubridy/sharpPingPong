679b875c8c6dd4cd9c8cd6ae594e02b48bfac694
Jessica Munoz and Joseph Tubridy <jessica@>
2019-07-15 15:41:10 -0700

    initial pingpong, working but not really the way we want

---
+++ PingPong.cs
@@ -0,0 +1,45 @@
+using System;
+
+class PingPong
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number");
+        string stringNumber = Console.ReadLine();
+        int userNumber = int.Parse(stringNumber); 
+        string[] stringArray = new string[userNumber+1];
+        for (int i = 1; i <= userNumber; i++)
+        {
+            if (i % 15 == 0)
+            {
+                stringArray[i] = "Ping-Pong";
+            }
+            else if (i % 3 == 0)
+            {
+                stringArray[i] = "Ping";
+            }
+            else if (i % 5 == 0)
+            {
+                stringArray[i]= "Pong";
+            }
+            else
+            {
+                stringArray[i]= i.ToString();
+            }
+            Console.WriteLine(string.Join("\n", stringArray));
+            // Console.WriteLine(stringArray);
+           // Console.WriteLine("[{0}]",string.Join(" ", stringArray));
+        }
+    }
+}
+
+// using System;
+
+// class PingPong
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Enter a number");
+//         string stringNumber = C
+//     }
+// }
